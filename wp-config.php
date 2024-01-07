3f41e0db63be79c17b790ac2d0732a0b60dea5d5
saurabh <saurabh.k@ehostinguk.com>
2024-01-07 15:09:02 +0530

    config

--- wp-config.php
+++ wp-config.php
@@ -22,13 +22,13 @@ define( 'WP_CACHE', true );
  */
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'indpush' );
+define( 'DB_NAME', 'u749958734_indpush' );
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u749958734_ind' );
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Ind@64487' );
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
