dec27e0408325e9c2bf1a144a69f9b659045fa5e
saurabh <saurabh.k@ehostinguk.com>
2024-01-07 15:24:58 +0530

    template

--- wp-content/themes/Indpush/index.php
+++ wp-content/themes/Indpush/index.php
@@ -8,6 +8,23 @@ Description: A brief description of your theme.
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.tooplate.com/templates/2135_mini_finance/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="https://www.tooplate.com/templates/2135_mini_finance/css/bootstrap-icons.css">
+
+<link rel="stylesheet" href="https://www.tooplate.com/templates/2135_mini_finance/css/apexcharts.css">
+
+<link rel="stylesheet" href="https://www.tooplate.com/templates/2135_mini_finance/css/tooplate-mini-finance.css">
+
+  <style>
+  /* Update the path to the local copy of the font */
+  @font-face {
+  font-family: 'bootstrap-icons';
+  src: url('/fonts/bootstrap-icons.woff2') format('woff2');
+  /* ... other font-face properties ... */
+  }
+
+  </style>
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
