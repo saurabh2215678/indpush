cba0fdf180a6ae924d93321f1b31ad07b3397d07
rohit <rohitsesodia@gmail.com>
2024-01-13 22:04:36 +0530

    new pull

--- IndpushFrontend/src/Pages/VerifyAccount.js
+++ IndpushFrontend/src/Pages/VerifyAccount.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../Redux/slices/user";
+
 
 const VerifyAccount = () => {
+
+    const dispatch = useDispatch();
+
+    const logOut=()=>{
+     dispatch(logout());
+    }
+
     return(
+        <>
         <div>Check Your Email and enter your Otp</div>
+        <button  onClick={logOut}>Logout</button>
+        </>
     )
 }
 export default VerifyAccount; 
